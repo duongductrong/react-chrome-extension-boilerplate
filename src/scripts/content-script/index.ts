204a0ec7f025f48d5feedeeed8aca987befd0ebd
duongductrong <duongductrong06@gmail.com>
2024-07-28 12:19:49 +0700

    chore(content.js): :fire: remove unneccessary code

--- src/scripts/content-script/index.ts
+++ src/scripts/content-script/index.ts
@@ -1,10 +1,5 @@
 /* eslint-disable no-console */
 
-import { browser } from "webextension-polyfill-ts"
 import "./renderer"
 
 console.log("🦄", "content script loaded")
-
-browser.action.onClicked.addListener(() => {
-  console.log("Hello")
-})
